cb4ee22f43fb8d22380343314c6a630c3519f72a
Reza Parseh <reza.parseh@aize.io>
2024-03-06 10:05:26 +0100

    working version

---
+++ run_docker.sh
@@ -0,0 +1,3 @@
+# docker run --name elasticsearch --net elastic -p 9200:9200 -p 9300:9300 -e "discovery.type=single-node" -t docker.elastic.co/elasticsearch/elasticsearch:8.9.1
+# docker run --name kibana --net elastic -p 5601:5601 docker.elastic.co/kibana/kibana:8.9.1
+docker run --rm -p 9200:9200 -p 9300:9300 -e "xpack.security.enabled=false" -e "discovery.type=single-node" docker.elastic.co/elasticsearch/elasticsearch:8.9.1
